bb4796817ed43ae1d66b4aef2493718342e117ac
Kristaps Eisaks <kristapseisaks03@gmail.com>
2024-07-13 08:25:39 +0100

    Progress update
    - nothing implemented just a progress update

--- Primero_Help_XD_Server/Program.cs
+++ Primero_Help_XD_Server/Program.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Net.Sockets;
 
 using System.Threading.Tasks;
+using System.Text.Json.Nodes;
 
 namespace Primero_Help_XD_Server
 {
@@ -17,10 +18,11 @@ namespace Primero_Help_XD_Server
     {
         private static TcpListener myListener;
         private static int port = 6969;
-        private static IPAddress localAddr = IPAddress.Parse("localhost");
+        private static IPAddress localAddr = IPAddress.Parse("0.0.0.0");
+
         static void Main(string[] args)
         {
-            myListener = new TcpListener(localAddr, port);
+            myListener = new TcpListener(localAddr,port);
             myListener.Start();
             Console.WriteLine($"Web Server Running on {localAddr.ToString()} on port {port}... Press ^C to Stop...");
             Thread th = new Thread(new ThreadStart(PrimeroServer));
@@ -29,10 +31,26 @@ namespace Primero_Help_XD_Server
         public static void PrimeroServer() {
             while (true)
             {
-                TcpClient client = myListener.AcceptTcpClient();
-                NetworkStream stream = client.GetStream();
+                using (TcpClient client = myListener.AcceptTcpClient())
+                using (NetworkStream stream = client.GetStream())
+                {
+                    StreamReader sr = new StreamReader(stream);
+                    string json = sr.ReadToEnd();
+                    Console.WriteLine(json);
+                    var Response = JsonConvert.DeserializeObject<string>(json);
 
-                client.Close();
+
+
+                    // Process the request (e.g., handle JSON deserialization)
+                    string response = $"Received: {Response}";
+
+                    // Send the response back to the client
+                    byte[] responseData = System.Text.Encoding.ASCII.GetBytes(response);
+                    stream.Write(responseData, 0, responseData.Length);
+                    Console.WriteLine($"Sent response: {response}");
+
+                    client.Close();
+                }
             }
         }
 
--- Primero_Help_XD_Server/obj/Debug/net8.0/Primero_Help_XD_Server.AssemblyInfo.cs
+++ Primero_Help_XD_Server/obj/Debug/net8.0/Primero_Help_XD_Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primero_Help_XD_Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2e3a19ed3fcfe474de1784f41539016c4f635af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primero_Help_XD_Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primero_Help_XD_Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
